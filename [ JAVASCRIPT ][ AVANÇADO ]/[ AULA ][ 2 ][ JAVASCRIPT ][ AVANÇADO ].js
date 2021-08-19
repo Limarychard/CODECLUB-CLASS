6f8864ec82104e65a2052e5e479dbe900a116be6
Limarychard <ry.lima2708@icloud.com>
2021-08-18 23:05:03 -0300

    advanced JavaScript lessons

---
+++ [ JAVASCRIPT ][ AVANÇADO ]/[ AULA ][ 2 ][ JAVASCRIPT ][ AVANÇADO ].js
@@ -0,0 +1,15 @@
+
+    // VAR x LET x CONST
+
+
+    if(true){
+
+        var myVar = "Hello" // NÃO USE VAR
+
+        let myLet = "Olá"
+        const myConst = "Hei"
+
+        console.log(myConst)
+    }
+
+    console.log(myVar)
---
+++ [ JAVASCRIPT ][ AVANÇADO ]/[ AULA ][ 3 ][ JAVASCRIPT ][ AVANÇADO ].js
@@ -0,0 +1,21 @@
+/*      
+
+    MÓDULO 
+
+    module.exports = ...
+
+
+*/ 
+
+const person = {
+    name: "Rychard",
+    age: 17
+}
+
+module.exports = person     // EXPORTANDO 
+
+// EM OUTRO ARQUIVO... 
+
+const personTest = require('./')    //
+ IMPORTANDO   
+
